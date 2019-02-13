eea957dac1596b973ee166c6e3cf6c7810dc9a1b
тимур50 <timurkadaas@gmail.com>
2019-02-13 23:43:18 +0200

    load properties

--- apps/common/src/main/java/ua/com/timur/common/Consts.java
+++ apps/common/src/main/java/ua/com/timur/common/Consts.java
@@ -6,30 +6,50 @@ package ua.com.timur.common;
 public class Consts {
 
 
-    public static final String APP_CONFIG_FILE_PATH = System.getProperty("app.config.file.path", "E:\\app.config");
-    public static final String TEST_PROP = getProperty("test.prop","");
+    public static final String APP_CONFIG_FILE_PATH = getSystemProperty("app.config.file.path", "E:\\app.config");
+    public static final String TEST_PROP = getSystemProperty("test.prop","");
+    public static final String APP_DB_URL = getSystemProperty("app.db.url","");
+    public static final String APP_DB_USER = getSystemProperty("app.db.user","");
+    public static final String APP_DB_PASSWORD = getSystemProperty("app.db.password","");
+
     public static final boolean SHOW_WELCOME_PAGE = getProperty("show.welcome.page", true);
     public static String MOBILE_ID_URL = getProperty("mobile.id.auth.url","");
-    public static String APP_DB_URL = getProperty("app.db.url","");
-    public static String APP_DB_USER = getProperty("app.db.user","");
-    public static String APP_DB_PASSWORD = getProperty("app.db.password","");
+    public static String MOBILE_ID_CLIENT_ID = getProperty("mobile.id.client_id","");
+    public static String MOBILE_ID_CLIENT_SECRET = getProperty("mobile.id.client_secret","");
     public static String APP_SIGN_ENCODING = getProperty("app.sign.encoding", "UTF-8");
-
-
+    public static boolean isLicenseReaded = false;
 
 
     public static String getProperty(String name, String defaultValue) {
-        return System.getProperty(name,defaultValue);
+        return PropertyLoader.getPropertyLoader().getProperty(name, defaultValue);
+    }
+
+    public static String getProperty(String name) {
+        return PropertyLoader.getPropertyLoader().getProperty(name);
     }
 
     public static boolean getProperty(String name, boolean defaultValue) {
-        String value = System.getProperty(name);
-        return value.isEmpty() ? defaultValue : Boolean.valueOf(value);
+        String value = getProperty(name);
+        return value != null ? Boolean.parseBoolean(value) : defaultValue;
     }
 
     public static int getProperty(String name, int defaultValue) {
+        String value = getProperty(name);
+        return value != null ? Integer.parseInt(value) : defaultValue;
+    }
+
+    public static String getSystemProperty(String name, String defaultValue) {
+        return System.getProperty(name,defaultValue);
+    }
+
+    public static boolean getSystemProperty(String name, boolean defaultValue) {
         String value = System.getProperty(name);
-        return value.isEmpty() ? defaultValue : Integer.parseInt(value);
+        return value != null ? defaultValue : Boolean.valueOf(value);
+    }
+
+    public static int getSystemProperty(String name, int defaultValue) {
+        String value = System.getProperty(name);
+        return value != null ? defaultValue : Integer.parseInt(value);
     }
 
 }
--- apps/common/src/main/java/ua/com/timur/common/PropertyLoader.java
+++ apps/common/src/main/java/ua/com/timur/common/PropertyLoader.java
@@ -1,9 +1,6 @@
 package ua.com.timur.common;
 
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,14 +8,27 @@ import java.util.Properties;
 /**
  * Created by тимур50 on 01.11.2018.
  */
-@Service
 public class PropertyLoader extends Properties {
 
+    private static PropertyLoader propertyLoader = null;
     private static Logger logger = Logger.getLogger(PropertyLoader.class);
 
-    @PostConstruct
-    public void load() throws IOException {
-        FileInputStream in = new FileInputStream(Consts.APP_CONFIG_FILE_PATH);
-        load(in);
+    private PropertyLoader() {
+        load();
+    }
+
+    public static PropertyLoader getPropertyLoader() {
+        if (propertyLoader == null)
+            propertyLoader = new PropertyLoader();
+        return propertyLoader;
+    }
+
+    private void load() {
+        try {
+            FileInputStream in = new FileInputStream(Consts.APP_CONFIG_FILE_PATH);
+            load(in);
+        } catch (Exception e) {
+            logger.fatal("Error while loading config file");
+        }
     }
 }
