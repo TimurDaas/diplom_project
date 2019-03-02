d9cfa2ab2401e567c3ce21a6003b5d48d87d3b46
тимур50 <timurkadaas@gmail.com>
2019-03-02 23:31:41 +0200

    fix httpInvoker, remoting-servlet

--- apps/business_logic/bl-startup/src/main/java/diplom/config/BLRemoteExportConfig.java
+++ apps/business_logic/bl-startup/src/main/java/diplom/config/BLRemoteExportConfig.java
@@ -1,9 +1,11 @@
 package diplom.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
 import ua.com.timur.interfaces.FirstBean;
+import ua.com.timur.services.FirstBeanImpl;
 
 /**
  * Created by тимур50 on 02.03.2019.
@@ -12,8 +14,9 @@ import ua.com.timur.interfaces.FirstBean;
 @Configuration
 public class BLRemoteExportConfig {
 
-    @Bean
-    public HttpInvokerServiceExporter firstBeanHttpService(FirstBean firstBean) {
+    @Bean("/firstBeanHttpService")
+    @Autowired
+    public HttpInvokerServiceExporter firstBeanHttpService(FirstBeanImpl firstBean) {
         HttpInvokerServiceExporter exporter = new HttpInvokerServiceExporter();
         exporter.setServiceInterface(FirstBean.class);
         exporter.setService(firstBean);
